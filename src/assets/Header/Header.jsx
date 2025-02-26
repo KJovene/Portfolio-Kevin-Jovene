50bbc926e1c99abc24afc87206dbc72a6c3e3c78
Kévin <jovene.kevin@gmail.com>
2025-02-26 11:58:49 +0100

    Changement d'image et création du Header

--- src/assets/Header/Header.jsx
+++ src/assets/Header/Header.jsx
@@ -4,7 +4,16 @@ import "./Header.css";
 const Header = () => {
   return (
     <div>
-
+      <header className="header">
+        <img src="../Img/Logo Blue.svg" className="logo" alt="Logo"/>
+        <h3>Portfolio de Kévin Jovené</h3>
+      </header>
+      <div className="navigation">
+        <button className="navigateur">Accueil</button>
+        <button className="navigateur">Projets</button>
+        <button className="navigateur">CV</button>
+      </div>
+      <hr />
     </div>
   );
 }
