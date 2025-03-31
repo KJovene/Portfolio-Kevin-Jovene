a0da5be85e0d56e762146922ca0f4d5a7b39827d
Kévin <jovene.kevin@gmail.com>
2025-03-31 17:38:16 +0200

    Final style AI Project

--- src/assets/Footer/Footer.jsx
+++ src/assets/Footer/Footer.jsx
@@ -5,15 +5,18 @@ import { Link } from 'react-router-dom';
 
 const Footer = () => {
   return (
-    <div className="footerContainer">
-      <div className='footer'>
-        <div className="linkFooter">
-          <a href="mailto:jovene.kevin@gmail.com">Me contacter</a>
+    <div>
+      <hr />
+      <div className="footerContainer">
+        <div className='footer'>
+          <div className="linkFooter">
+            <a href="mailto:jovene.kevin@gmail.com">Me contacter</a>
+          </div>
+          <Link to='/CV'>
+            <Bouton text="Voir mon CV" />
+          </Link>
+          <img src="../../../Img/Logo Blue.svg" alt="Logo" />
         </div>
-        <Link to='/CV'>
-          <Bouton text="Voir mon CV" />
-        </Link>
-        <img src="../../../Img/Logo Blue.svg" alt="Logo" />
       </div>
     </div>
   );
